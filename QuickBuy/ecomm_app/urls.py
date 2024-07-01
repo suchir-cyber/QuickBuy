4e8f8de3206a3a29e8b71b53a0c3872aa29c14ab
suchir-cyber <suchirpandula@gmail.com>
2024-07-01 15:06:51 +0530

    first commit

---
+++ QuickBuy/ecomm_app/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index,name="index"),
+    path('contact/',views.contact,name="contact"),
+    path('about/',views.about,name="about"),
+]
