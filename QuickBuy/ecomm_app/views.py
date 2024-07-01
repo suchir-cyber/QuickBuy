a7dccf82dc579e5ca78f8a72bf3b59933b5fc051
suchir-cyber <suchirpandula@gmail.com>
2024-07-02 04:46:16 +0530

    added contacts section

--- QuickBuy/ecomm_app/views.py
+++ QuickBuy/ecomm_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from ecomm_app.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -8,4 +10,15 @@ def about(request):
     return render(request,"about.html")
 
 def contact(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        desc=request.POST.get("desc")
+        pnumber=request.POST.get("pnumber")
+        myquery=Contact(name=name,email=email,desc=desc,phonenumber=pnumber)
+        myquery.save()
+        messages.info(request,"we recieved your request, so we will get back to you soon..")
+        return render(request,"contact.html")
+
+
     return render(request,"contact.html")
